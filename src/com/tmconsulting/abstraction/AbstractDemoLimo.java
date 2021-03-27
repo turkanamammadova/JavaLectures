6f0a8306258c3a03b636c2836ecad684750d6980
turkanamammadova <evamammadova@gmail.com>
2021-03-27 14:32:21 -0500

    new class is added to abstraction package

---
+++ src/com/tmconsulting/abstraction/AbstractDemoLimo.java
@@ -0,0 +1,22 @@
+package com.tmconsulting.abstraction;
+
+public class AbstractDemoLimo extends AbstractCars{
+    //utilize abstract class
+    public void setPrivateSpeed(int speed){
+        super.setPrivateSpeed(10); //calling super class method
+    }
+
+    public void engineStart(){
+        super.engineStart();
+        System.out.println("BMW have keyless engine start");
+
+        //we have to inherit abstract method otherwise it will give error
+
+    }
+//this is limo class
+    @Override
+    public void engineStart2(String keyType, int numofGears) {
+        System.out.println("This is BMW abstract method");
+    }
+
+}
